088e706c037fd1b6587e97d6272d144a5abb4dc7
liamshen10 <liamshen03@gmail.com>
2023-08-13 23:03:58 -0400

    testing

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -21,7 +21,7 @@ export const logout = async () => {
   return response;
  };
  export const updateUser = async (credentials, user) => {
-  console.log(user._id)
+  console.log('User ID: ' + user._id)
   const response = await api.put(`${USERS_URL}/${user._id}`, { credentials, user });
   console.log(response);
   return response.data;

797bf1b98df50b74582f137171f3bc082810dff0
liamshen10 <liamshen03@gmail.com>
2023-08-15 10:45:14 -0400

    fixing

--- src/tuiter/user/profile-screen.js
+++ src/tuiter/user/profile-screen.js
@@ -23,36 +23,37 @@ function ProfileScreen() {
 
 return (
   <div>
-   <h1>Profile Screen</h1>
-   {profile && (<div>
-     <div>
-      <label>First Name</label>
-      <input type="text" value={profile.firstName}
-       onChange={(event) => {
-        const newProfile = {
-         ...profile, firstName: event.target.value,
-        };
-        setProfile(newProfile);
-       }}/>
-     </div>
-     <div>
-      <label>Last Name</label>
-      <input type="text" value={profile.lastName}
-       onChange={(event) => {
-        const newProfile = {
-         ...profile, lastName: event.target.value,
-        };
-        setProfile(newProfile);
-       }}/>
-     </div></div>
-   )}
-   <button
-    onClick={() => {
-      dispatch(logoutThunk());
-      navigate("/tuiter/login");
-    }}>                   Logout</button>
-   <button onClick={save}>Save  </button>
-  </div> );
+    <h1>Profile Screen</h1>
+    {profile && (<div>
+      <div className="mt-2">
+        <label>First Name</label>
+        <input className="form-control" type="text" value={profile.firstName ?? ""}
+          onChange={(event) => {
+            const newProfile = {
+              ...profile, firstName: event.target.value,
+            };
+            setProfile(newProfile);
+          }}
+        />
+      </div>
+      <div className="mt-2">
+        <label>Last Name</label>
+        <input className="form-control" type="text" value={profile.lastName ?? ""}
+          onChange={(event) => {
+            const newProfile = {
+              ...profile, lastName: event.target.value,
+            };
+            setProfile(newProfile);
+          }} />
+      </div></div>
+    )}
+    <button className="btn btn-primary mt-2 me-2"
+      onClick={() => {
+        dispatch(logoutThunk());
+        navigate("/tuiter/login");
+      }}>Logout</button>
+    <button className="btn btn-primary mt-2" onClick={save}>Save  </button>
+  </div>);
 }
 export default ProfileScreen;
  
